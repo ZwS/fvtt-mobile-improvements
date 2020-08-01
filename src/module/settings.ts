201cdf0df6cc5188413c409f098257a8acd24322
Sunspots <sunspots@sunspots.eu>
2020-08-02 00:42:09 +0200

    update settings framework

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,17 +1,47 @@
 const MODULE_NAME = "mobile-improvements"; // TODO: Better handling
 
-export const settings = {
-  RENDERMODES: "renderModes",
+export enum settings {
+  RENDERMODES = "renderModes",
+}
+
+interface Callbacks {
+  [setting: string]: (value) => void;
+}
+
+const noop = (): void => {
+  return;
 };
 
-export function registerSettings() {
-  game.settings.register(MODULE_NAME, settings.RENDERMODES, {
+const moduleSettings = [
+  {
+    setting: settings.RENDERMODES,
     name: "Render mode toggle",
     hint:
       "Adds a button below the sidebar to turn off scene rendering (reload required)",
     type: Boolean,
     default: false,
-    scope: "client",
+  },
+];
+
+function registerSetting(callbacks: Callbacks, { setting, ...options }): void {
+  game.settings.register(MODULE_NAME, setting, {
     config: true,
+    scope: "client",
+    ...options,
+    onChange: callbacks[setting] || noop,
   });
+}
+
+export function registerSettings(callbacks: Callbacks = {}): void {
+  moduleSettings.forEach(item => {
+    registerSetting(callbacks, item);
+  });
+}
+
+export function getSetting(setting: settings): string | boolean | number {
+  return game.settings.get(MODULE_NAME, setting as string);
+}
+
+export function setSetting(setting: settings, value): Promise<any> {
+  return game.settings.set(MODULE_NAME, setting as string, value);
 }
